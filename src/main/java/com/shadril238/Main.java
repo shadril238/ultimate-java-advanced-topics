29a9612c17b663aef951c91d9c50d25e79e16567
shadril238 <shadril.hassan@brainstation-23.com>
2024-09-18 19:10:10 +0600

    Java exceptions

--- src/main/java/com/shadril238/Main.java
+++ src/main/java/com/shadril238/Main.java
@@ -2,8 +2,6 @@ package com.shadril238;
 
 import com.shadril238.exceptions.ExceptionsDemo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("---Ultimate Java Advanced Topics---");
